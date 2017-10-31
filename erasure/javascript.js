a5eb5cae41d21b19ba9ffab486ed8cf81763db3b
admin <admin@admins-MBP.home>
2017-10-31 01:35:36 -0400

    updated with hw

---
+++ erasure/javascript.js
@@ -0,0 +1,18 @@
+alert("welcome to javascript")
+
+var hi= 45
+
+alert(hi)
+
+// // document.getElementById("id_you_want")
+// document.getElementClassName(name_of_the_class")
+
+
+
+var catImage = document.querySelector("#fixed_img")
+
+catImage.addEventListener("click",showpopup)
+
+function showpopup(){
+	alert("you clicked it!")
+}
---
+++ treehole/javascript.js
@@ -0,0 +1,16 @@
+alert("Welcome to Treehole, a haven for those emotions about a certain someone that seem to haunt you. Click close to begin. ")
+
+var sign = prompt("Who is he/she?");
+
+var sign = prompt("Describe him/her.");
+
+var sign = prompt("How did you meet?");
+
+var sign = prompt("What is your most memorable experience together?");
+
+var sign = prompt("What did you dislike most about him/her?");
+
+var sign = prompt("What is one your biggest regret regarding him/her?");
+
+var sign = prompt("What yould you like to say to him/her?");
+
